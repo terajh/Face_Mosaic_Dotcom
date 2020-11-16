5cec15eab184627009ea6a9166eba06b3f1d6395
ParkJooHyun <terajoohyun@ajou.ac.kr>
2020-11-16 19:29:03 +0900

    UI modifying

--- src/Component/section/aside/Aside.jsx
+++ src/Component/section/aside/Aside.jsx
@@ -10,26 +10,25 @@ const Aside = () => {
             onSelect={selectedKey => alert(`selected ${selectedKey}`)}
         >
             <div className="sidebar-sticky"></div>
-            <div class="leftsidebar_accordion_panel show" id="img_fn_list_panel">
+            <div className="leftsidebar_accordion_panel show" id="img_fn_list_panel">
                 <div id="project_info_panel">
-                    <div class="row">
-                        <span class="col">Name:</span>
-                        <span class="col">
-                            <input type="text" value="" onchange="project_on_name_update(this)" id="project_name" title="VIA project name" />
+                    <div className="rows">
+                        <span className="cols">Name:</span>
+                        <span className="cols">
+                            <input type="text" id="project_name" title="VIA project name" />
                         </span>    
                     </div>
                 </div>
                 <div id="project_tools_panel">
-                    <div class="button_panel">
-                        <select id="filelist_preset_filters_list" onchange="img_fn_list_onpresetfilter_select()" title="Filter file list using predefined filters">
-                            <option value="all">All files</option>
+                    <div className="button_panel">
+                        <select id="filelist_preset_filters_list" title="Filter file list using predefined filters">
+                            <option value="all">All Faces</option>
                             <option value="files_without_region">Show files without regions</option>
                             <option value="files_missing_region_annotations">Show files missing region annotations</option>
                             <option value="files_missing_file_annotations">Show files missing file annotations</option>
                             <option value="files_error_loading">Files that could not be loaded</option>
                             <option value="regex">Regular Expression</option>
                         </select>
-                        <input type="text" placeholder="regular expression" oninput="img_fn_list_onregex()" id="img_fn_list_regex" title="Filter using regular expression"/>
                     </div>
                 </div>
 
